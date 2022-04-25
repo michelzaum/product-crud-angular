03bf990d5db274401bdfc9b15ee793d457aa5955
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-04-24 23:43:58 -0300

    Creating navigation for the product creation page, using event binding

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-crud',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor() { }
+  // Aqui está sendo utilizado a injeção de dependência.
+  // Dessa forma, o Angular irá ser o responsável por instanciar a classe
+  // e nos fornecer as informações da tipagem do 'Router' no momento que
+  // o componente 'app-product-crud' for referenciado. Com isso, podemos
+  // utilizar este objeto com seus métodos dentro desta classe.
+  constructor(private router: Router) { };
 
   ngOnInit(): void {
-  }
+  };
 
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create']);
+  };
 }
