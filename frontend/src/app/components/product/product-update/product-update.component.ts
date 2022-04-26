537faa8f85482a23d94877c7ad68805876104266
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-04-26 11:48:16 -0300

    Creating funcionality to update products

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
+  product: Product = {
+    name: '',
+    price: 0,
+  };
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { };
 
   ngOnInit(): void {
-  }
+    const id = this.route.snapshot.paramMap.get('id'); // obtendo o id vindo da rota.
+    this.productService.readById(String(id)).subscribe(product => {
+      this.product = product;
+    });
+  };
 
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  };
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  };
 }
